3b78ac4c659c74092b0abb19882302bd19a7f56a
mostafa <mostafameerzad@gmail.com>
2024-06-20 15:00:40 +0430

    Fix bill input issue

--- src/components/BillInput.tsx
+++ src/components/BillInput.tsx
@@ -4,13 +4,14 @@ import { useTip } from "../context/TipContext";
 
 export const BillInput = () => {
   const { bill, setBill, calculateTip } = useTip();
+
   useEffect(() => {
     if (bill > 0) calculateTip();
   }, [bill]);
+
   return (
     <div className="bill">
       <h2 className="title">bill</h2>
-
       <div
         className={`flex px-6 py-3 rounded-lg bg-very-light-grayish-cyan sm:py-2 focus-within:outline-strong-cyan focus-within:outline `}
       >
@@ -19,10 +20,9 @@ export const BillInput = () => {
           type="number"
           value={bill === 0 ? "" : bill}
           placeholder="0"
-          className="w-full bg-transparent text-very-dark-cyan text-2xl text-right outline-none sm:text-xl"
-          onChange={(e) => {
-            setBill(parseFloat(e.target.value));
-          }}
+          pattern="[0-9]+"
+          className="w-full bg-transparent text-very-dark-cyan text-2xl text-right outline-none sm:text-xl no-spinner"
+          onChange={(e) => setBill(parseFloat(e.target.value))}
         />
       </div>
     </div>
