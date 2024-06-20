ffe477ee8f6013c8c2d50200e441816d407e3365
mostafa <mostafameerzad@gmail.com>
2024-06-20 15:01:17 +0430

    Animations the results

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,16 +1,37 @@
+import { useEffect } from "react";
 import { useTip } from "../context/TipContext";
+import { useSpring, animated } from "@react-spring/web";
 
 export const Results = () => {
   const { reset, totalCost, tipAmount } = useTip();
 
+  const [animatedTipAmount, apiTipAmount] = useSpring(() => ({
+    number: 0,
+    from: { number: 0 },
+  }));
+  const [animatedTotalCost, apiTotalCost] = useSpring(() => ({
+    number: 0,
+    from: { number: 0 },
+  }));
+
+  useEffect(() => {
+    apiTipAmount.start({ number: parseFloat(tipAmount) });
+    apiTotalCost.start({ number: parseFloat(totalCost) });
+  }, [tipAmount, totalCost, apiTipAmount, apiTotalCost]);
+
   return (
-    <div className=" bg-very-dark-cyan rounded-2xl p-8 flex flex-col  gap-3 w-full min-h-80 max-w-md ">
+    <div className=" bg-very-dark-cyan rounded-2xl p-8 flex flex-col  gap-3 w-full min-h-80">
       <div className="result-wrapper">
         <div>
           <h2 className="result-title">tip amount</h2>
           <p className=" result-sub-title">/ person</p>
         </div>
-        <h2 className="result ">${tipAmount}</h2>
+        <h2 className="result ">
+          <animated.span>
+            {animatedTipAmount.number.to((n) => `$${n.toFixed(2)}`)}
+          </animated.span>
+          {}
+        </h2>
       </div>
 
       <div className="result-wrapper">
@@ -18,7 +39,11 @@ export const Results = () => {
           <h2 className="result-title">total</h2>
           <p className=" result-sub-title">/ person</p>
         </div>
-        <h2 className="result ">${totalCost}</h2>
+        <h2 className="result ">
+          <animated.span>
+            {animatedTotalCost.number.to((n) => `$${n.toFixed(2)}`)}
+          </animated.span>
+        </h2>
       </div>
 
       <button
